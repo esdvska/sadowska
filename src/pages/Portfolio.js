34007273a6e878eb567c13470640ec078866f440
Ewelina Sadowska <esdvska@gmail.com>
2021-02-05 21:17:42 +0100

    react image gallery

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "../components/photos";
 
 const Portfolio = () => {
-  return <p>PORTFOLIO</p>;
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <div className="portfolio">
+      <Gallery photos={photos} direction={"column"} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
 };
 export default Portfolio;
