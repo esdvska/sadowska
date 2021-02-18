413162f7ecd3941facaf2d50e734cfb94add6d2a
Ewelina Sadowska <esdvska@gmail.com>
2021-02-18 11:47:05 +0100

    add .htaccess

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import { TiSocialFacebook } from "react-icons/ti";
 import { TiDevicePhone } from "react-icons/ti";
